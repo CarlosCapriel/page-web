ef2dd2c78a9eb935317ab67b9357d73fba50a158
Carlos-Capriel <ccapriel@auronix.com>
2021-09-17 11:54:13 -0500

    se creo el componente para registro

---
+++ views/registro.php
@@ -0,0 +1,47 @@
+<div class="container">
+  <div class="row">
+    <form class="grey darken-2 col s6">
+      <div class="row">
+        <div class="col s6">
+          <label for="first_name col s12">Nombre</label>
+        </div>
+        <div class="input-field col s12">
+          <input placeholder="Nombre" id="first_name" type="text" class="validate">
+        </div>
+        <div class="col s12"><label for="last_name">Apellidos</label></div>
+          <div class="input-field col s6">
+            <input id="last_name" type="text" class="validate" placeholder="Apellido Paterno">
+          </div>
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate" placeholder="Apellido Materno">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+            <label for="password">Contraseña</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate" placeholder="Contraseña">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12"><label for="email">Email</label></div>
+          <div class="input-field col s12">
+            <input id="email" type="email" class="validate">
+          </div>
+        </div>
+        <div class="row">  
+          <label class="col s8">
+            <input type="checkbox" class="filled-in" checked="checked" />
+            <span>Acepto los terminos y condiciones</span>
+          </label>
+        </div>
+        <div class="row">
+          <div class="col s10">
+            <a class="waves-effect waves-light btn">Aceptar</a>
+          </div>
+       </div>
+    </form>
+  </div>
+</div>
+<script type="text/javascript" src="js/materialize.min.js"></script>
